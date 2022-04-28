5018e70ddc0a22e87763fdecaf49585a4b2bde7a
Alvaro Franco <alvarofmk@gmail.com>
2022-04-28 12:42:42 +0200

    Cambiado proyecto a Spring

---
+++ PhoenixKarting/src/main/java/controladores/Controlador.java
@@ -0,0 +1,10 @@
+package controladores;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Controlador {
+	
+	
+
+}
---
+++ PhoenixKarting/src/test/java/com/salesianostriana/dam/phoenixkart/PhoenixKartingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.phoenixkart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoenixKartingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
