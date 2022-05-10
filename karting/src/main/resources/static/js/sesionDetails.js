bd4168b54447224f9c295b8aa4f3e5be1d98512d
Alvaro Franco <alvarofmk@gmail.com>
2022-05-10 09:05:28 +0200

    Función Javascript añadida para enseñar vueltas del piloto seleccionado

---
+++ karting/src/main/resources/static/js/sesionDetails.js
@@ -0,0 +1,15 @@
+let selectorPiloto = document.getElementById("pilotoVueltas");
+
+selectorPiloto.addEventListener('change',showLaps);
+
+function showLaps(evento){
+	let tablasVueltas = document.querySelectorAll("#tablaVueltas tbody");
+	
+	for (let tabla of tablasVueltas){
+		if(tabla.id == evento.currentTarget.value){
+			tabla.hidden = false;
+		}else{
+			tabla.hidden = true;
+		}
+	}
+}
