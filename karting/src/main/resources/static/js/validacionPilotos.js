a97d1151de64adeb1f142e76516d739a5bef6837
Álvaro <alvarofmk@gmail.com>
2022-05-16 17:07:54 +0200

    Validacion pilotos hecha

--- karting/src/main/resources/static/js/validacionPilotos.js
+++ karting/src/main/resources/static/js/validacionPilotos.js
@@ -4,45 +4,106 @@
 	document.getElementById("imgPiloto").addEventListener("blur",comprobarImg);
 	document.getElementById("emailPiloto").addEventListener("blur",comprobarEmail);
 	document.getElementById("fechaNacimiento").addEventListener("blur",comprobarEdad);
-
+	
+	document.querySelectorAll(".errorForm").forEach(p => p.hidden = true);
 
 function validarFormulario(){
 	let resultado = false;
 
-	resultado = comprobarEdad();
+	resultado = comprobarEdad() &&
+				comprobarNombre() &&
+				comprobarApellidos() &&
+				comprobarDni() &&
+				comprobarImg() &&
+				comprobarEmail();
 
 	return resultado;
 }
 
 function comprobarNombre(){
+	let nombre = formPilotos.nombrePiloto;
+	let correcto = nombre.value!="";
 	
+	if(correcto){
+		nombre.nextElementSibling.hidden = true;
+	}else{
+		nombre.nextElementSibling.hidden = false;
+	}
+	
+	return correcto;
 }
 
 function comprobarApellidos(){
+	let apellidos = formPilotos.apellidosPiloto;
+	let correcto = apellidos.value!="";
 	
+	if(correcto){
+		apellidos.nextElementSibling.hidden = true;
+	}else{
+		apellidos.nextElementSibling.hidden = false;
+	}
+	
+	return correcto;
 }
 function comprobarDni(){
+	let dni = formPilotos.dniPiloto;
+	let correcto = dni.value!="";
 	
+	if(correcto){
+		
+		dni.value = dni.value.toUpperCase();
+		let numDNI = dni.value.substring(0,8); 
+		let letraDNI = dni.value.substring(8);
+
+		let letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J',
+		 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
+
+		correcto = (numDNI > 0 && numDNI < 99999999) && (letras[numDNI%23]==letraDNI);
+		
+		if(correcto){
+			dni.nextElementSibling.hidden = true;
+		}else{
+			dni.nextElementSibling.hidden = false;
+		}
+	}
+	
+	
+	return correcto;
 }
 function comprobarImg(){
-	
+	return true;
 }
 function comprobarEmail(){
+	let corrreo = formPilotos.emailPiloto;
+	let correcto = corrreo.value!="";
 	
+	if(correcto){
+		corrreo.nextElementSibling.hidden = true;
+	}else{
+		corrreo.nextElementSibling.hidden = false;
+	}
+	
+	return correcto;
 }
 function comprobarEdad(){
 	let fecha = formPilotos.fechaNacimiento;
-	let resultado = fecha.value!="";
+	let correcto = fecha.value!="";
 	
-	if(resultado){
+	if(correcto){
 		let fechaDate = fecha.valueAsDate;
 		let hoy = new Date();
 
 		let anyos = hoy.getYear()-fechaDate.getYear();		
 		
-		resultado = anyos>=6;
+		correcto = anyos>=6;
 		
 	}
 	
-	return resultado;
+	if(correcto){
+		fecha.nextElementSibling.hidden = true;
+	}else{
+		fecha.nextElementSibling.hidden = false;
+	}
+	
+	return correcto;
 }
